3c56643fbadbf00be748087a1a92e053ccc03854
Rami Hashan <DXroomram@gmail.com>
2022-11-11 23:52:23 +0200

    Changed line ending + added comments

--- VectorDistanceCalculator.h
+++ VectorDistanceCalculator.h
@@ -9,8 +9,25 @@ namespace VectorDistanceCalculator {
 	
 	double manhattan(const Vector v1, const Vector v2);
 	
+	/**
+	 * @brief Calculates the Chebyshev distance of two vectors.
+	 * The Chebyshev distance is the maximum of the distances in each one of the axis.
+	 * 
+	 * @param v1 The first vector.
+	 * @param v2 The second vector.
+	 * @return The Chebyshev distance of the two vectors.
+	 */
 	double chebyshev(const Vector v1, const Vector v2);
 	
+	/**
+	 * @brief Calculates the Canberra distance of the two vectors.
+	 * The Canberra distance is the sum of |pi-qi|/(|pi|-|qi|) where pi and qi are the values of vectors
+	 * p and q in the i-th axis.
+	 * 
+	 * @param v1 The first vector.
+	 * @param v2 The second vector.
+	 * @return The Canberra distance of the two vectors.
+	 */
 	double canberra(const Vector v1, const Vector v2);
 	
 	double minkowski(const Vector v1, const Vector v2, const int p);
