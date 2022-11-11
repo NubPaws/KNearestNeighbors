22e9db0c5ab791ff1d8b2b7da4732d9614898218
Rami Hashan <DXroomram@gmail.com>
2022-11-11 22:42:15 +0200

    Added main function + readme

--- Program.cpp
+++ Program.cpp
@@ -4,6 +4,19 @@
 
 int main() {
 	
+	double x, y, z;
 	
+	std::cin >> x >> y >> z;
+	Vector v1(x, y, z);
 	
+	std::cin >> x >> y >> z;
+	Vector v2(x, y, z);
+	
+	std::cout << VectorDistanceCalculator::euclidean(v1, v2) << std::endl
+			<< VectorDistanceCalculator::manhattan(v1, v2) << std::endl
+			<< VectorDistanceCalculator::chebyshev(v1, v2) << std::endl
+			<< VectorDistanceCalculator::canberra(v1, v2) << std::endl
+			<< VectorDistanceCalculator::minkowski(v1, v2, 1) << std::endl;
+	
+	return 0;
 }
