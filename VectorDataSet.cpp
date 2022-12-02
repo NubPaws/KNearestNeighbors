b7f249cf8e750846deffc87943216b44964d6834
RanWurm <ranwurmbrand@gmail.com>
2022-12-02 20:52:18 +0200

    a little code to vectordataset class :)

--- VectorDataSet.cpp
+++ VectorDataSet.cpp
@@ -1,3 +1,13 @@
 #include "VectorDataSet.h"
 
+class VectorDataSet {
+    typedef std::pair<std::vector<double>, std::string> DataEntry;
 
+private:
+    std::vector<DataEntry> D_EntryVectors;
+
+public:
+    VectorDataSet(std::istream file){
+
+    }
+};
